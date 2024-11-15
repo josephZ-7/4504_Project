729ff3b5be7a015ef62fd3fcf41b39779718c141
ZacharyCarruthers <144139268+ZacharyCarruthers@users.noreply.github.com>
2024-11-15 17:05:40 -0500

    Update SThread.java

--- main/SThread.java
+++ main/SThread.java
@@ -1,64 +1,68 @@
 import java.net.*;
 import java.io.*;
+import java.util.List;
 
 public class SThread extends Thread {
 	private Socket clientSocket;
 	private Socket serverSocket;
-	private Object[][] routingTable;
 
-	private PrintWriter clientOut;
-	private BufferedReader clientIn;
+	private ObjectOutputStream clientOut;
+	private ObjectInputStream clientIn;
 
-	private PrintWriter serverOut;
-	private BufferedReader serverIn;
+	private ObjectOutputStream serverOut;
+	private ObjectInputStream serverIn;
+
 
 	public SThread(Socket clientSocket, Socket serverSocket) {
 		this.clientSocket = clientSocket;
 		this.serverSocket = serverSocket;
-		this.routingTable = routingTable;
 	}
 
 	public void run() {
 		try {
-			clientOut = new PrintWriter(clientSocket.getOutputStream(), true);
-			clientIn = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+			clientOut = new ObjectOutputStream(clientSocket.getOutputStream());
+			clientIn = new ObjectInputStream(clientSocket.getInputStream());
 
-			String fromClient;
-			String toClient;
+			List<int[][]> fromClient;
+			int[][] toClient;
 
-			serverOut = new PrintWriter(serverSocket.getOutputStream(), true);
-			serverIn = new BufferedReader(new InputStreamReader(serverSocket.getInputStream()));
+			serverOut = new ObjectOutputStream(serverSocket.getOutputStream());
+			serverIn = new ObjectInputStream(serverSocket.getInputStream());
 
-			String fromServer;
-			String toServer;
+			int[][] fromServer;
+			List<int[][]> toServer;
 
-			fromClient = clientIn.readLine();
+			fromClient = (List<int[][]>) clientIn.readObject();
 
 			if(fromClient != null){
-				System.out.println("Thread: Received message from Client -> " + fromClient);
+				System.out.println("Thread: Received message from Client.");
 			}
 
 			toServer = fromClient;
 
-			System.out.println("Thread: Forwarding message to Server ->" + toServer);
-			serverOut.println(toServer);
+			System.out.println("Thread: Forwarding message to Server.");
+			serverOut.writeObject(toServer);
 
 			/////
 
-			fromServer = serverIn.readLine();
+			fromServer = (int[][]) serverIn.readObject();
 
 			if (fromServer != null) {
-				System.out.println("Thread: Received message from Server -> " + fromServer);
+				System.out.println("Thread: Received response from Server.");
 			}
 
 			toClient = fromServer;
 
-			System.out.println("Thread: Forwarding message to Client -> " + toClient);
-			clientOut.println(toClient);
+			System.out.println("Thread: Forwarding response to Client.");
+			clientOut.writeObject(toClient);
+
+
 		} catch (IOException e) {
 			System.err.println("Error in communication.");
 			e.printStackTrace();
-		} finally {
+		} catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        } finally {
 			try {
 				clientIn.close();
 				clientOut.close();
